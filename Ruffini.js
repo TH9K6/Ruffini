c84823dbf43201fee0b66313794f69bf2709e349
TH9K6 <thomas9k6@outlook.com>
2024-11-11 18:08:51 +0100

    Update Ruffini.js

--- Ruffini.js
+++ Ruffini.js
@@ -1,6 +1,6 @@
 document.getElementById('trova').addEventListener('click', function() {
     const expression = document.getElementById('espressione').value;
-    const formattedExpression = expression.replace(/\^/g, '**').replace(/x/gi, 'x');
+    const formattedExpression = expression.replace(/\^/g, '**');
     findZero(formattedExpression);
 });
 
@@ -15,47 +15,48 @@ function simplifyFraction(numerator, denominator) {
 
 function findZero(expression) {
     const espressione = expression;
-    const exp = expression.split(' ');
+    const exp = expression.split(/\s*(?=[+\-])/g);
     const p = [];
     const q = [];
     const numeratori = [];
     const denominatori = [];
     const zeri = [];
     const zeriPossibili = new Set();
+    const zeriEval = [];
 
     exp.forEach(piece => {
         piece = piece.replace(/\+/g, '').replace(/-/g, '');
         if (piece.match(/^x/i)) {
             q.push('1');
         } else if (!piece.match(/x/i)) {
-            p.push(`-${piece}`, piece);
+            p.push(piece);
         } else {
             q.push(piece.split('*')[0]);
         }
     });
 
     p.forEach(number => {
-        for (let divisore = 1; divisore <= parseInt(number); divisore++) {
-            if (parseInt(number) % divisore === 0) {
-                numeratori.push(divisore.toString(), `-${divisore}`);
+        for (let divisore = 1; divisore <= Math.abs(number); divisore++) {
+            if (Math.abs(number) % divisore === 0) {
+                numeratori.push(divisore, -divisore);
             }
         }
     });
 
     q.forEach(number => {
-        for (let divisore = 1; divisore <= parseInt(number); divisore++) {
-            if (parseInt(number) % divisore === 0) {
-                denominatori.push(divisore.toString());
+        for (let divisore = 1; divisore <= Math.abs(number); divisore++) {
+            if (Math.abs(number) % divisore === 0) {
+                denominatori.push(divisore);
             }
         }
     });
 
     numeratori.forEach(numeratore => {
         denominatori.forEach(denominatore => {
-            if (denominatore === '1') {
+            if (denominatore === 1) {
                 zeriPossibili.add(`${numeratore}`);
             } else {
-                const [simpNum, simpDen] = simplifyFraction(parseInt(numeratore), parseInt(denominatore));
+                const [simpNum, simpDen] = simplifyFraction(numeratore, denominatore);
                 zeriPossibili.add(`${simpNum}/${simpDen}`);
             }
         });
@@ -63,20 +64,20 @@ function findZero(expression) {
 
     Array.from(zeriPossibili).forEach(zero => {
         let result = 0;
-        let espressioneTemp = espressione;
-        if (!espressioneTemp.match(/x/i)) {
-            espressioneTemp = espressioneTemp.replace(new RegExp(`${zero}`, 'gi'), `${zero}`);
-        }
-        espressioneTemp = espressioneTemp.replace(/x/gi, `(${zero})`);
-        const index = Array.from(zeriPossibili).indexOf(zero);
-        espressioneTemp = espressioneTemp.split(' ');
-        espressioneTemp.forEach(piece => {
-            result += Math.round(eval(piece) * 10000) / 10000;
-        });
+        let espressioneTemp = espressione.replace(/x/g, `(${zero})`).replace(/\^/g, '**');
+        result = eval(espressioneTemp);
         if (result === 0) {
             zeri.push(zero);
         }
         if (zeri.filter(z => z === zero).length === 2) {
+            zeri.splice(zeri.indexOf(zero), 1);
+        }
+    });
+
+    zeri.forEach(zero => {
+        zeriEval.push(eval(zero));
+        const zeroEval = eval(zero);
+        if (zeriEval.filter(z => z === zeroEval).length > 1) {
             zeri.splice(zeri.indexOf(zero), 1);
         }
     });
@@ -89,7 +90,6 @@ function findZero(expression) {
         resultElement.appendChild(para);
     });
 }
-
 
 function changeTheme() {
     let CSSlink = document.querySelector("#stylesheet");
